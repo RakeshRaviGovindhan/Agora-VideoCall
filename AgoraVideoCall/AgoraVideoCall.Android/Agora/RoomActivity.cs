8f31e0d8461fabf982a25d73474998c8afdbb14e
RakeshRaviGovindhan <rakeshravig001@gmail.com>
2021-06-10 23:25:28 +0530

    Bug fixed - Included Mono.Android.Export.dll

---
+++ AgoraVideoCall/AgoraVideoCall.Android/Agora/RoomActivity.cs
@@ -0,0 +1,258 @@
+﻿using AgoraVideoCall.Agora;
+using AgoraVideoCall.Agora.Helpers;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using DT.Xamarin.Agora;
+using DT.Xamarin.Agora.Video;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgoraVideoCall.Droid.Agora
+{
+    [Activity(Label = "VideoChat")]
+    public class RoomActivity : Activity
+    {
+        protected const int MaxLocalVideoDimension = 150;
+        protected RtcEngine AgoraEngine;
+        protected AgoraRtcHandler AgoraHandler;
+        private bool _isVideoEnabled = true;
+        private SurfaceView _localVideoView;
+        private uint _localId = 0;
+        private uint _remoteId = 0;
+        private ProgressBar _progressBar;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            //SetContentView(Resource.Layout.Room);
+            //_progressBar = FindViewById<ProgressBar>(Resource.Id.progress_bar);
+            InitAgoraEngineAndJoinChannel();
+            //FindViewById<TextView>(Resource.Id.room_name).Text = AgoraSettings.Current.RoomName;
+        }
+
+
+        public void OnFirstRemoteVideoDecoded(int uid, int width, int height, int elapsed)
+        {
+            RunOnUiThread(() =>
+            {
+                SetupRemoteVideo(uid);
+            });
+        }
+
+        public void OnUserOffline(int uid, int reason)
+        {
+            RunOnUiThread(() =>
+            {
+                OnRemoteUserLeft();
+            });
+        }
+
+        public void OnUserMuteVideo(int uid, bool muted)
+        {
+            RunOnUiThread(() =>
+            {
+                OnRemoteUserVideoMuted(uid, muted);
+            });
+        }
+
+        public void OnJoinChannelSuccess(string channel, int uid, int elapsed)
+        {
+            RunOnUiThread(() => _progressBar.Visibility = ViewStates.Gone);
+            _localId = (uint)uid;
+            RefreshDebug();
+        }
+
+        public void RefreshDebug()
+        {
+            RunOnUiThread(() =>
+            {
+                //FindViewById<TextView>(Resource.Id.debug_data).Text = $"local: {_localId}\nremote: {_remoteId}";
+            });
+        }
+
+        public void OnFirstLocalVideoFrame(float height, float width, int p2)
+        {
+            var ratio = height / width;
+            var ratioHeight = ratio * MaxLocalVideoDimension;
+            var ratioWidth = MaxLocalVideoDimension / ratio;
+            var containerHeight = height > width ? MaxLocalVideoDimension : ratioHeight;
+            var containerWidth = height > width ? ratioWidth : MaxLocalVideoDimension;
+            RunOnUiThread(() =>
+            {
+                //var videoContainer = FindViewById<RelativeLayout>(Resource.Id.local_video_container);
+                //var parameters = videoContainer.LayoutParameters;
+                //parameters.Height = (int)TypedValue.ApplyDimension(ComplexUnitType.Dip, containerHeight, Resources.DisplayMetrics);
+                //parameters.Width = (int)TypedValue.ApplyDimension(ComplexUnitType.Dip, containerWidth, Resources.DisplayMetrics);
+                //videoContainer.LayoutParameters = parameters;
+                //FindViewById(Resource.Id.local_video_container).Visibility = _isVideoEnabled ? ViewStates.Visible : ViewStates.Invisible;
+            });
+        }
+
+        private void InitAgoraEngineAndJoinChannel()
+        {
+            InitializeAgoraEngine();
+            SetupVideoProfile();
+            SetupLocalVideo();
+            JoinChannel();
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+        }
+
+        [Java.Interop.Export("OnLocalVideoMuteClicked")]
+        public void OnLocalVideoMuteClicked(View view)
+        {
+            ImageView iv = (ImageView)view;
+            if (iv.Selected)
+            {
+                iv.Selected = false;
+                iv.SetImageResource(Resource.Drawable.ic_cam_active_call);
+            }
+            else
+            {
+                iv.Selected = true;
+                iv.SetImageResource(Resource.Drawable.ic_cam_disabled_call);
+            }
+            AgoraEngine.MuteLocalVideoStream(iv.Selected);
+            _isVideoEnabled = !iv.Selected;
+            //FindViewById(Resource.Id.local_video_container).Visibility = _isVideoEnabled ? ViewStates.Visible : ViewStates.Gone;
+            //_localVideoView.Visibility = _isVideoEnabled ? ViewStates.Visible : ViewStates.Gone;
+        }
+
+        [Java.Interop.Export("OnLocalAudioMuteClicked")]
+        public void OnLocalAudioMuteClicked(View view)
+        {
+            ImageView iv = (ImageView)view;
+            if (iv.Selected)
+            {
+                iv.Selected = false;
+                iv.SetImageResource(Resource.Drawable.ic_mic_active_call);
+            }
+            else
+            {
+                iv.Selected = true;
+                iv.SetImageResource(Resource.Drawable.ic_mic_inactive_call);
+            }
+            AgoraEngine.MuteLocalAudioStream(iv.Selected);
+            //var visibleMutedLayers = iv.Selected ? ViewStates.Visible : ViewStates.Invisible;
+            //FindViewById(Resource.Id.local_video_overlay).Visibility = visibleMutedLayers;
+            //FindViewById(Resource.Id.local_video_muted).Visibility = visibleMutedLayers;
+        }
+
+        [Java.Interop.Export("OnSwitchCameraClicked")]
+        public void OnSwitchCameraClicked(View view)
+        {
+            AgoraEngine.SwitchCamera();
+        }
+
+        [Java.Interop.Export("OnEncCallClicked")]
+        public void OnEncCallClicked(View view)
+        {
+            AgoraEngine.StopPreview();
+            AgoraEngine.SetupLocalVideo(null);
+            AgoraEngine.LeaveChannel();
+            AgoraEngine.Dispose();
+            AgoraEngine = null;
+            Finish();
+        }
+
+        public async Task OnTokenPrivilegeWillExpire(string token)
+        {
+            var newToken = await AgoraTokenService.GetRtcToken(AgoraSettings.Current.RoomName);
+            if (!string.IsNullOrEmpty(token))
+            {
+                AgoraEngine.RenewToken(newToken);
+            }
+        }
+
+        private void InitializeAgoraEngine()
+        {
+            AgoraHandler = new AgoraRtcHandler(this);
+            AgoraEngine = RtcEngine.Create(BaseContext, AgoraTestConstants.AgoraAPI, AgoraHandler);
+        }
+
+        private void SetupVideoProfile()
+        {
+            AgoraEngine.EnableVideo();
+            AgoraEngine.SetVideoProfile(AgoraSettings.Current.UseMySettings ? AgoraSettings.Current.Profile : Constants.VideoProfile360p, false);
+        }
+
+        private void SetupLocalVideo()
+        {
+            //FrameLayout container = (FrameLayout)FindViewById(Resource.Id.local_video_view_container);
+            //_localVideoView = RtcEngine.CreateRendererView(BaseContext);
+            //_localVideoView.SetZOrderMediaOverlay(true);
+            //container.AddView(_localVideoView);
+            AgoraEngine.SetupLocalVideo(new VideoCanvas(_localVideoView, VideoCanvas.RenderModeAdaptive, 0));
+            if (!string.IsNullOrEmpty(AgoraSettings.Current.EncryptionPhrase))
+            {
+                AgoraEngine.SetEncryptionMode(AgoraSettings.Current.EncryptionType.GetModeString());
+                AgoraEngine.SetEncryptionSecret(AgoraSettings.Current.EncryptionPhrase);
+            }
+            AgoraEngine.StartPreview();
+        }
+
+        private async Task JoinChannel()
+        {
+            _progressBar.Visibility = ViewStates.Visible;
+            var token = await AgoraTokenService.GetRtcToken(AgoraSettings.Current.RoomName);
+            if (string.IsNullOrEmpty(token))
+            {
+                _progressBar.Visibility = ViewStates.Gone;
+            }
+            else
+            {
+                AgoraEngine.JoinChannel(token, AgoraSettings.Current.RoomName, string.Empty, 0); // if you do not specify the uid, we will generate the uid for you
+            }
+        }
+
+        private void SetupRemoteVideo(int uid)
+        {
+            //_remoteId = (uint)uid;
+            //FrameLayout container = (FrameLayout)FindViewById(Resource.Id.remote_video_view_container);
+            //if (container.ChildCount >= 1)
+            //{
+            //    return;
+            //}
+            //SurfaceView surfaceView = RtcEngine.CreateRendererView(BaseContext);
+            //container.AddView(surfaceView);
+            //AgoraEngine.SetupRemoteVideo(new VideoCanvas(surfaceView, VideoCanvas.RenderModeAdaptive, uid));
+            //surfaceView.Tag = uid; // for mark purpose
+            //RefreshDebug();
+        }
+
+        private void LeaveChannel()
+        {
+            AgoraEngine.LeaveChannel();
+        }
+
+        private void OnRemoteUserLeft()
+        {
+            //FrameLayout container = (FrameLayout)FindViewById(Resource.Id.clremote_video_view_container);
+            //container.RemoveAllViews();
+        }
+
+        private void OnRemoteUserVideoMuted(int uid, bool muted)
+        {
+            //FrameLayout container = (FrameLayout)FindViewById(Resource.Id.remote_video_view_container);
+            //SurfaceView surfaceView = (SurfaceView)container.GetChildAt(0);
+            //if (surfaceView == null)
+            //    return;
+            //var tag = surfaceView.Tag;
+            //if (tag != null && (int)tag == uid)
+            //{
+            //    surfaceView.Visibility = muted ? ViewStates.Gone : ViewStates.Visible;
+            //}
+        }
+    }
+}
